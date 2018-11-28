dfe22c7f392fc033b7a437c74235920d19e9f581
jongjin <dmer11@naver.com>
2018-11-29 00:29:00 +0900

    변동사항 없음

--- app/src/main/java/com/example/administrator/simplememo/MainActivity.java
+++ app/src/main/java/com/example/administrator/simplememo/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.administrator.simplememo;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,24 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClick_setting_costume_save(View view) {
-        new AlertDialog.Builder(this)
-                .setTitle("Delete entry")
-                .setMessage("정말 이걸 삭제할건가요?")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 이 버튼 클릭시 삭제 진행
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 아무일도 안 일어남
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
-    }
-
-
-
 }
