add9a042b3894ae6e61c4878dc206785522aab80
MoonJongJin <weakcountry@gmail.com>
2018-11-29 15:04:08 +0900

    메인 액티비티 수정

--- app/src/main/java/com/example/administrator/simplememo/MainActivity.java
+++ app/src/main/java/com/example/administrator/simplememo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.simplememo;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,24 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClick_setting_costume_save(View view) {
-        new AlertDialog.Builder(this)
-                .setTitle("Delete entry")
-                .setMessage("정말 이걸 삭제할건가요?")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 이 버튼 클릭시 삭제 진행
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 아무일도 안 일어남
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
+
+    public void mOnClick(View v) {
+        Intent intent;
+        switch(v.getId()) {
+            case R.id.btn_new :
+                intent = new Intent(this, Edit.class );
+                intent.putExtra("수 정", false);
+                startActivity(intent);
+                break;
+            case R.id.btn_open:
+                intent = new Intent(this, OpenList.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_exit:
+                finish();
+                break;
+        }
     }
-
-
-
 }
